6ca4fca7f3e3b0d57d5179d4569dcf6d51201169
Kaladin <onpalm@gmail.com>
2024-09-13 02:06:27 +0300

    test tap

--- Formula/cubtera.rb
+++ Formula/cubtera.rb
@@ -1,17 +1,33 @@
-# TODO: create github action in main repo for update this formula to be dynamic
 class Cubtera < Formula
   desc "Immersive cubic dimensions experience."
   homepage "https://github.com/cubtera/cubtera"
-  head "https://github.com/cubtera/cubtera.git"
-  url "https://github.com/cubtera/cubtera/releases/download/v0.1.0/cubtera-mac-aarch64.tar.gz"
-  sha256 "72825c6e5ca0be20082aa1c7f5ac98f0a90cb95be2f07c2f2216014f5fbb86c3"
-  version "0.1.0"
+  version "0.2.0"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/cubtera/cubtera/releases/download/v0.1.0/cubtera-darwin-intel.zip"
+      sha256 "" # Intel macOS
+    else
+      url "test/test/test/cubtera-darwin-aarch64.zip"
+      sha256 "4b7cd379cff3f0661d851a0f032d93423865e490d15a7e8bd83b86d9d02e9797" # M1 macOS
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/cubtera/cubtera/releases/download/v0.1.0/cubtera.zip"
+      sha256 "a0f6fc1860ba5a3cc3542d9661bdfdcd72e9d5355d026a9c10dd2271f5eb1c02"
+    else
+      url "https://github.com/cubtera/cubtera/releases/download/v0.1.0/cubtera.zip"
+      sha256 "a0f6fc1860ba5a3cc3542d9661bdfdcd72e9d5355d026a9c10dd2271f5eb1c02"
+
+  end
 
   def install
-    bin.install "target/aarch64-apple-darwin/release/cubtera" => "cubtera"
+    bin.install "cubtera" #=> "cubtera"
   end
 
   test do
-    assert_match "cubtera 0.1.0", shell_output("#{bin}/cubtera --version", 2)
+    assert_match "cubtera 0.2.0", shell_output("#{bin}/cubtera --version", 2)
   end
 end
