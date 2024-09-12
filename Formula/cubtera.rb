e95a7987226a6a6ae15fcff07013f8623e9bccec
Denis <den.zalman76@gmail.com>
2024-09-12 22:47:20 +0300

    Update cubtera.rb
    update with details

--- Formula/cubtera.rb
+++ Formula/cubtera.rb
@@ -3,15 +3,15 @@ class Cubtera < Formula
   desc "Immersive cubic dimensions experience."
   homepage "https://github.com/cubtera/cubtera"
   head "https://github.com/cubtera/cubtera.git"
-  url "*mac.tar.gz" # ADD proper link
-  sha256 "<ADD SHA>"
-  version "4.0.0"
+  url "https://github.com/cubtera/cubtera/releases/download/v0.1.0/cubtera-mac-aarch64.tar.gz"
+  sha256 "72825c6e5ca0be20082aa1c7f5ac98f0a90cb95be2f07c2f2216014f5fbb86c3"
+  version "0.1.0"
 
   def install
     bin.install "cubtera"
   end
 
   test do
-    assert_match "cubtera version 4.0.0", shell_output("#{bin}/cubtera --version", 2)
+    assert_match "cubtera 0.1.0", shell_output("#{bin}/cubtera --version", 2)
   end
 end
