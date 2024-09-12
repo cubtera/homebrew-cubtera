7aceee30825860eb298064786ba42553decbaa35
Denis <den.zalman76@gmail.com>
2024-09-12 22:57:05 +0300

    Update cubtera.rb

--- Formula/cubtera.rb
+++ Formula/cubtera.rb
@@ -8,7 +8,7 @@ class Cubtera < Formula
   version "0.1.0"
 
   def install
-    bin.install "terget/aarch64-apple-darwin/release/cubtera"
+    bin.install "target/aarch64-apple-darwin/release/cubtera"
   end
 
   test do
